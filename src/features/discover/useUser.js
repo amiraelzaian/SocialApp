0a5bd049b0c776f9742df2c42d7771109c80da98
AZ <amiraelzaian204@gmail.com>
2026-02-14 02:46:41 +0200

    create files

---
+++ src/features/discover/useUser.js
@@ -0,0 +1,1 @@
+//# useQuery to fetch suggested users
--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,5 +1,5 @@
 import Sidebar from "./Sidebar";
-import Navbar from "./Navbar";
+import Navbar from "./Header";
 
 function AppLayout() {
   return (
