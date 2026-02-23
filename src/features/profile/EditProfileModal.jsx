3af451e359aa86485e7421c143c325b22590d15e
AZ <amiraelzaian204@gmail.com>
2026-02-23 22:25:30 +0200

    handle files

---
+++ src/features/profile/EditProfileModal.jsx
@@ -0,0 +1,5 @@
+function EditProfileModal() {
+  return <div></div>;
+}
+
+export default EditProfileModal;
---
+++ src/features/profile/ProfileHeader.jsx
@@ -0,0 +1,7 @@
+// contain cover, avatar and info
+
+function ProfileHeader() {
+  return <div></div>;
+}
+
+export default ProfileHeader;
