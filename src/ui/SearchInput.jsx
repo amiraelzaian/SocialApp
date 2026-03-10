6a11eb85fecef77258286fe699084ecd67ae9e9e
AZ <amiraelzaian204@gmail.com>
2026-03-10 23:47:38 +0200

    remove query from search input

--- src/ui/SearchInput.jsx
+++ src/ui/SearchInput.jsx
@@ -2,6 +2,30 @@ import Input from "./Input.jsx";
 import { useSearch } from "../context/SearchContext.jsx";
 import { useGetPage } from "../hooks/useGetPage.js";
 import { useEffect } from "react";
+import { HiXCircle } from "react-icons/hi2";
+
+import styled from "styled-components";
+
+const StyledDiv = styled.div`
+  position: relative;
+  width: 100%;
+`;
+
+const XButton = styled.button`
+  position: absolute;
+  right: 10px;
+  top: 50%;
+  transform: translateY(-50%);
+
+  outline: none;
+  background-color: transparent;
+
+  &:hover,
+  &:active,
+  &:focus {
+    outline: none;
+  }
+`;
 
 const placeholders = {
   Discover: "Search users...",
@@ -18,12 +42,19 @@ function SearchInput() {
   }, [nameOfPage, setSearchQuery]);
 
   return (
-    <Input
-      type="text"
-      placeholder={placeholders[nameOfPage] || "Search..."}
-      value={searchQuery}
-      onChange={(e) => setSearchQuery(e.target.value)}
-    />
+    <StyledDiv>
+      <Input
+        type="text"
+        placeholder={placeholders[nameOfPage] || "Search..."}
+        value={searchQuery}
+        onChange={(e) => setSearchQuery(e.target.value)}
+      />
+      {searchQuery && (
+        <XButton onClick={() => setSearchQuery("")}>
+          <HiXCircle size={22} />
+        </XButton>
+      )}
+    </StyledDiv>
   );
 }
 
